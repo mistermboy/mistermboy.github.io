b7476912ae0660343b41ba3860c7313372fb391e
mistermboy <pabloyo97@hotmail.com>
2020-08-04 18:15:24 +0200

    follow mouse animation

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload = function(){
 
-    let animationBtn = document.getElementById('activateAnimation');
+/*     let animationBtn = document.getElementById('activateAnimation');
     animationBtn.addEventListener("click", function(){
         let lSquares = document.getElementsByClassName('l-square');
         let counter = 0;
@@ -18,5 +18,23 @@ window.onload = function(){
         })
   
     });
+ */
+
+    window.addEventListener('mousemove', function(e) {
+        var xPos = e.pageX;
+        var yPos = e.pageY;
+
+        if(xPos>1200)xPos = 1200;
+        let leftPupil = document.getElementById('leftPupil');
+        let rightPupil = document.getElementById('rightPupil');
+        let top  = (yPos/10) - 46+'px';
+        let left = (xPos/10) - 56+'px';
+
+        leftPupil.style.top = top
+        leftPupil.style.left= left
+        rightPupil.style.top = top
+        rightPupil.style.left= left
+      });
+
 
 }
