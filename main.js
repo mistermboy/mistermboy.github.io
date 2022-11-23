ec1d5a827ff173d59e54518a2a4857ea04995102
mistermboy <pabloyo97@hotmail.com>
2022-11-23 10:08:35 +0100

    finally eyes working

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-//This is a pen based off of Codewoofy's eyes follow mouse. It is just cleaned up, face removed, and then made to be a little more cartoony. https://codepen.io/Codewoofy/pen/VeBJEP
+/*https://codepen.io/shantanu-jana/pen/yLprQjR*/
 
-$(".main-container").mousemove(function(event) {
-  var eye = $(".eye");
-  console.log('eye', eye)
-  var x = (eye.offset().left) + (eye.width() / 2);
-  var y = (eye.offset().top) + (eye.height() / 2);
-  var rad = Math.atan2(event.pageX - x, event.pageY - y);
-  var rot = (rad * (180 / Math.PI) * -1) + 180;
-  eye.css({
-    '-webkit-transform': 'rotate(' + rot + 'deg)',
-    '-moz-transform': 'rotate(' + rot + 'deg)',
-    '-ms-transform': 'rotate(' + rot + 'deg)',
-    'transform': 'rotate(' + rot + 'deg)'
+document.querySelector("body").addEventListener("mousemove", eyeball);
+
+function eyeball() {
+  const eye = document.querySelectorAll(".eye");
+  eye.forEach(function (eye) {
+    let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
+    let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
+
+    let radian = Math.atan2(event.pageX - x, event.pageY - y);
+    let rotate = radian * (180 / Math.PI) * -1 + 270;
+    eye.style.transform = "rotate(" + rotate + "deg)";
   });
-});
+}
